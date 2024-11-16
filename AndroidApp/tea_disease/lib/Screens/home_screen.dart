666942c684f84fc2350e143ddd6df3a779a8e50e
Santosh Bhandari <santoshvandari1@gmail.com>
2024-11-16 16:34:50 +0545

    Futter APK Initialized

---
+++ AndroidApp/tea_disease/lib/Screens/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DiseaseHomePage extends StatelessWidget {
+  const DiseaseHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
